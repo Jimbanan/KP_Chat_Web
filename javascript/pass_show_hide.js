8017cd7cdc51f1f653a98db0963cfc221d24567e
Jimbanan <uservice371@mail.ru>
2022-03-24 21:01:06 +0300

    Start JS

---
+++ javascript/pass_show_hide.js
@@ -0,0 +1,12 @@
+// const pswrdField = document.querySelector(".form input[type='password']"),
+// toggleBtn = document.querySelector(".form .field i");
+//
+// toggleBtn.onclick = () => {
+//     alert();
+// }
+
+// Собственно сам код
+document.querySelector('#signup-sumbit').onclick = function (event) {
+    event.preventDefault();
+    alert();
+}
